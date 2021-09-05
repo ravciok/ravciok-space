612afdbf238b9cf1f14480dad266147b80778585
Rafał Ciok <ravciok@gmail.com>
2021-09-05 20:41:33 +0200

    comments script added to rendererd static files

--- scully.ravciok-space.config.ts
+++ scully.ravciok-space.config.ts
@@ -1,4 +1,4 @@
-import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { HandledRoute, registerPlugin, ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import 'prismjs/components/prism-markup.js';
 import 'prismjs/components/prism-scss.js';
 import 'prismjs/components/prism-typescript.js';
@@ -6,12 +6,30 @@ import 'prismjs/components/prism-json.js';
 
 const { DisableAngular } = require('scully-plugin-disable-angular');
 
-const postRenderers = [DisableAngular];
+function addBlogComments(dom: any, route: HandledRoute | undefined): Promise<any> {
+  if (route?.route.match(/^\/blog\/.+/)) {
+    dom.window.document.getElementById('commentsContainer').innerHTML = `
+      <script
+        src="https://utteranc.es/client.js"
+        repo="ravciok/blog-comments"
+        issue-term="pathname"
+        theme="github-light"
+        crossorigin="anonymous"
+        async
+      ></script>`;
+  }
+
+  return Promise.resolve(dom);
+}
+
+registerPlugin('postProcessByDom', 'commentsScript', addBlogComments);
 
 setPluginConfig('md', { enableSyntaxHighlighting: true });
 setPluginConfig(DisableAngular, 'postProcessByHtml', {
   removeState: true,
 });
+
+const postRenderers = [DisableAngular, 'commentsScript'];
 
 export const config: ScullyConfig = {
   projectRoot: './src',
--- src/app/blog/components/blog-post/blog.component.ts
+++ src/app/blog/components/blog-post/blog.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Renderer2, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
 import { Title } from '@angular/platform-browser';
 import { shareReplay, take } from 'rxjs/operators';
@@ -10,9 +10,7 @@ import { shareReplay, take } from 'rxjs/operators';
   preserveWhitespaces: true,
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class BlogComponent implements AfterViewInit {
-  @ViewChild('comments') commentsContainer: any;
-
+export class BlogComponent {
   public data$ = this.routes.getCurrent().pipe(shareReplay(1));
 
   private url: string = window.location.href;
@@ -24,35 +22,9 @@ export class BlogComponent implements AfterViewInit {
   public facebookLink = `https://www.facebook.com/sharer/sharer.php?u=${this.url}&quote=${this.title}`;
   public linkedinLink = `https://www.linkedin.com/sharing/share-offsite/?url=${this.url}&title=${this.title}`;
 
-  constructor(private routes: ScullyRoutesService, private titleService: Title, private renderer: Renderer2) {}
-
-  public ngAfterViewInit() {
-    this.setTitle();
-    this.addCommentsScript();
-  }
-
-  private setTitle(): void {
+  constructor(private routes: ScullyRoutesService, private titleService: Title) {
     this.data$.pipe(take(1)).subscribe((data) => {
       this.titleService.setTitle(data.title as string);
     });
-  }
-
-  private addCommentsScript(): void {
-    const script = this.renderer.createElement('script');
-
-    const attributes = {
-      src: 'https://utteranc.es/client.js',
-      repo: 'ravciok/blog-comments',
-      'issue-term': 'pathname',
-      theme: 'github-light',
-      crossorigin: 'anonymous',
-      async: true,
-    };
-
-    for (const [key, value] of Object.entries(attributes)) {
-      script.setAttribute(key, value);
-    }
-
-    this.renderer.appendChild(this.commentsContainer.nativeElement, script);
   }
 }
